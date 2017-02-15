9340f03fbc39c2932a6fbc3360dc376fb510be95
Eito Katagiri <eitoball@gmail.com>
2017-02-15 14:04:17 +0900

    add image upload feature

--- web/models/idea.ex
+++ web/models/idea.ex
@@ -1,10 +1,11 @@
 defmodule Phoenixhuman.Idea do
   use Phoenixhuman.Web, :model
+  use Arc.Ecto.Schema
 
   schema "ideas" do
     field :name, :string
     field :description, :string
-    field :picture, :string
+    field :picture, Phoenixhuman.Picture.Type
 
     timestamps()
   end
@@ -15,6 +16,7 @@ defmodule Phoenixhuman.Idea do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:name, :description, :picture])
+    |> cast_attachments(params, [:picture], [])
     |> validate_required([:name, :description, :picture])
   end
 end
